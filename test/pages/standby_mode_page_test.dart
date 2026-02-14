8402044fdd441789d2387a5b976a9792a7ea44da
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2026-02-13 20:19:55 -0500

    add basic standby mode tests

---
+++ test/pages/standby_mode_page_test.dart
@@ -0,0 +1,54 @@
+// Dart imports:
+
+// Flutter imports:
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// Package imports:
+import 'package:flutter_test/flutter_test.dart';
+// Project imports:
+import 'package:waterboard/pages/standby_mode_page.dart';
+import 'package:waterboard/services/ros_comms/ros.dart';
+import 'package:waterboard/widgets/ros_connection_state_widget.dart';
+import 'package:waterboard/widgets/time_text.dart';
+
+import '../test_helpers/test_util.dart';
+
+Future<void> pumpPage(WidgetTester widgetTester, ROS ros) async {
+  FlutterError.onError = ignoreOverflowErrors;
+  await widgetTester.pumpWidget(MaterialApp(home: StandbyModePage(ros: ros)));
+}
+
+void main() {
+  FlutterError.onError = ignoreOverflowErrors;
+  TestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('Verify Correct Widgets', (widgetTester) async {
+    await pumpPage(
+      widgetTester,
+      createFakeROS(initialState: ROSConnectionState.connected),
+    );
+    expect(find.byType(ClockText), findsOneWidget);
+    expect(find.byType(ROSConnectionStateWidget), findsOneWidget);
+  });
+  group("Closing Page", () {
+    testWidgets('Test Closing Keybind', (widgetTester) async {
+      await pumpPage(
+        widgetTester,
+        createFakeROS(initialState: ROSConnectionState.connected),
+      );
+      await widgetTester.sendKeyEvent(LogicalKeyboardKey.escape);
+      await widgetTester.pumpAndSettle();
+      expect(find.byType(StandbyModePage), findsNothing);
+    });
+    testWidgets('Test Close Button', (widgetTester) async {
+      await pumpPage(
+        widgetTester,
+        createFakeROS(initialState: ROSConnectionState.connected),
+      );
+      var closeButton = find.byIcon(Icons.arrow_back_rounded);
+      expect(closeButton, findsOneWidget);
+      await widgetTester.tap(closeButton);
+      await widgetTester.pumpAndSettle();
+      expect(find.byType(StandbyModePage), findsNothing);
+    });
+  });
+}
