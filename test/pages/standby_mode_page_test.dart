8cd86bc77f2148d0bbb67a7228f7b219e6362b25
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2026-02-13 20:26:16 -0500

    add standby mode slides tests

--- test/pages/standby_mode_page_test.dart
+++ test/pages/standby_mode_page_test.dart
@@ -3,14 +3,15 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 // Package imports:
 import 'package:flutter_test/flutter_test.dart';
+
 // Project imports:
 import 'package:waterboard/pages/standby_mode_page.dart';
 import 'package:waterboard/services/ros_comms/ros.dart';
 import 'package:waterboard/widgets/ros_connection_state_widget.dart';
 import 'package:waterboard/widgets/time_text.dart';
-
 import '../test_helpers/test_util.dart';
 
 Future<void> pumpPage(WidgetTester widgetTester, ROS ros) async {
@@ -26,6 +27,31 @@ void main() {
       widgetTester,
       createFakeROS(initialState: ROSConnectionState.connected),
     );
+    expect(find.byType(ClockText), findsOneWidget);
+    expect(find.byType(ROSConnectionStateWidget), findsOneWidget);
+
+    await widgetTester.sendKeyEvent(LogicalKeyboardKey.arrowRight);
+    await widgetTester.pumpAndSettle();
+
+    expect(find.byType(ROSConnectionStateWidget), findsOneWidget);
+    expect(find.text("Our Sponsors"), findsOneWidget);
+    expect(find.text("Platinum "), findsOneWidget);
+    expect(find.text("Sponsors"), findsOneWidget);
+    expect(find.text("Sponsor"), findsNWidgets(2));
+    expect(find.text("Gold "), findsOneWidget);
+    expect(find.text("Silver "), findsOneWidget);
+    expect(find.text("American Society of\n Naval Engineers"), findsOneWidget);
+    expect(find.text("Private Sponsor"), findsOneWidget);
+    expect(
+      find.text(
+        "Society of Naval Architects and Marine Engineers\nStevens Chapter",
+      ),
+      findsOneWidget,
+    );
+    expect(find.text("DHX Machines"), findsOneWidget);
+
+    await widgetTester.sendKeyEvent(LogicalKeyboardKey.arrowRight);
+    await widgetTester.pumpAndSettle();
     expect(find.byType(ClockText), findsOneWidget);
     expect(find.byType(ROSConnectionStateWidget), findsOneWidget);
   });
